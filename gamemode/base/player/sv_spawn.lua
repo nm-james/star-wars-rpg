d63ad6b24371f27d2bfdce9ad4d664660fd59ab2
nm-james <nathanjmanley04@gmail.com>
2021-11-10 16:40:24 +1100

    Update sv_spawn.lua

--- gamemode/base/player/sv_spawn.lua
+++ gamemode/base/player/sv_spawn.lua
@@ -4,5 +4,6 @@ function GM:PlayerSpawn(ply, transition)
     print(ply, transition)
     ply:SetPos( Vector( 4097, -5771, 12905 ) )
     ply:SetAngles( Angle( 0, 180, 0 ) )
-    ply:SetModel("models/jajoff/sps/alpha/tc13j/coloured_regular04.mdl")
+    ply:SetModel("models/jajoff/sps/alpha/tc13j/coloured_regular01.mdl")
+    ply:SetPlayerColor( Color( 255, 255, 255 ) )
 end
