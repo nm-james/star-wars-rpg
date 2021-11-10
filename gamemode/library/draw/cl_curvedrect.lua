06f06f3a9bbe7ea70890eb09bc7117fcc66f95b4
Nathan Manley (Falcon) <nathanjmanley04@gmail.com>
2021-11-10 16:18:06 +1100

    Update #1 - 3

--- gamemode/library/draw/cl_curvedrect.lua
+++ gamemode/library/draw/cl_curvedrect.lua
@@ -1,9 +1,9 @@
 Falcon = Falcon or {}
-
+local color_black = Color( 0, 0, 0, 250 )
 local curvedRect = Material("f_coop/curved_square.png")
 function surface.DrawCurvedRect( x, y, h, w, rotation, color )
     draw.NoTexture()
-    surface.SetDrawColor( color or Color( 255, 255, 255, 255 ) )
+    surface.SetDrawColor( color or color_black )
     surface.SetMaterial(curvedRect)
     surface.DrawTexturedRectRotated( x, y, w, h, (rotation or 0) )
     draw.NoTexture()
