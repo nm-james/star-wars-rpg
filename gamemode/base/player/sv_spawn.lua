5093013b3c61712f4d60b836679a1104fa9a1912
nm-james <nathanjmanley04@gmail.com>
2021-11-10 16:38:19 +1100

    Update sv_spawn.lua

--- gamemode/base/player/sv_spawn.lua
+++ gamemode/base/player/sv_spawn.lua
@@ -1,1 +1,8 @@
 Falcon = Falcon or {}
+
+function GM:PlayerSpawn(ply, transition)
+    print(ply, transition)
+    ply:SetPos( Vector( 4097, -5771, 12905 ) )
+    ply:SetAngles( Angle( 0, 180, 0 ) )
+    ply:SetModel("models/jajoff/sps/alpha/tc13j/coloured_regular04.mdl")
+end
